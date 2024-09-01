17d45196aa3da801fbb34862f4f5cbab958b8c79
alessandrobarbosa <alessandro.jr.b@hotmail.com>
2024-08-31 21:39:24 -0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+def caminhoRED(arvore, no):
+    print(no , end=" ")
+    # Se tiver filhos
+    if arvore[no] != None:
+        # Filho a esquerda
+        caminhoRED(arvore, arvore[no][0])
+        # Filho a direita
+        if len(arvore[no]) > 1:
+            caminhoRED(arvore, arvore[no][1])
+
 def exercicioA():
-    return None
+    print("Caminho em pré-ordem: ")
+    caminhoRED(arvoreTeste, 'A')
 
 def exercicioB():
     return None
@@ -9,3 +20,20 @@ def exercicioC():
 
 def exercicioD():
     return None
+
+arvoreTeste = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['G'],
+    'D': None,
+    'E': ['F'],
+    'F': None,
+    'G': None
+}
+
+exercicioA()
+
+# entrada = input("Insira a arvore no formato: 'A': ['B', 'C'], 'B': ['A', 'D']\n")
+# grafo = dict(eval("{" + entrada + "}"))
+# for x in grafo.values():
+#     print(x)
