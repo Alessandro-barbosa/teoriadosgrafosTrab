28dc4b886f52965bbac06f61c88e97303baec0b0
alessandrobarbosa <alessandro.jr.b@hotmail.com>
2024-09-05 06:33:31 -0300

    atualização caminhos ordem, pos, nova arvore

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 def caminhoRED(arvore, no):
-    print(no , end=" ")
-    # Se tiver filhos
+    print(no, end= " ")
     if arvore[no] != None:
-        # Filho a esquerda
-        caminhoRED(arvore, arvore[no][0])
-        # Filho a direita
-        if len(arvore[no]) > 1:
+        if arvore[no][0] != None:
+            caminhoRED(arvore, arvore[no][0])
+        if arvore[no][1] != None:
             caminhoRED(arvore, arvore[no][1])
+
+def ordem(arvore, no):
+    if arvore[no] != None:
+        if arvore[no][0] != None:
+            ordem(arvore, arvore[no][0])  
+            print(no, end = " ")
+        if(arvore[no][1] != None):
+            ordem(arvore, arvore[no][1])    
+    else:
+        print(no, end = " ")
+
+def posOrdem(arvore, no):
+    if arvore[no] != None:
+        if arvore[no][0] != None:
+            posOrdem(arvore, arvore[no][0])            
+        if arvore[no][1] != None:
+            posOrdem(arvore, arvore[no][1])
+        print(no, end = " ") 
+    else:
+        print(no, end= " ")
 
 def exercicioA():
     print("Caminho em pré-ordem: ")
     caminhoRED(arvoreTeste, 'A')
+    print("\nCaminho em Ordem: ")
+    ordem(arvoreTeste, 'A')
+    print("\nCaminho em pós-ordem")
+    posOrdem(arvoreTeste, 'A')
 
 def exercicioB():
     return None
@@ -24,15 +46,40 @@ def exercicioD():
 arvoreTeste = {
     'A': ['B', 'C'],
     'B': ['D', 'E'],
-    'C': ['G'],
+    'C': ['G', None],
     'D': None,
-    'E': ['F'],
+    'E': ['F', None],
     'F': None,
     'G': None
 }
 
-exercicioA()
+arvoreTeste2 = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['G', 'H'],
+    'D': ['I', 'J'],
+    'E': ['K', 'L'],
+    'F': ['M', 'N'],
+    'G': ['O', 'P'],
+    'H': ['Q', 'R'],
+    'I': ['S', None],
+    'J': None,
+    'K': None,
+    'L': ['T', None],
+    'M': [None, 'U'],
+    'N': None,
+    'O': ['V', None],
+    'P': None,
+    'Q': [None, 'W'],
+    'R': None,
+    'S': None,
+    'T': None,
+    'U': None,
+    'V': None,
+    'W': None
+}
 
+exercicioA()
 # entrada = input("Insira a arvore no formato: 'A': ['B', 'C'], 'B': ['A', 'D']\n")
 # grafo = dict(eval("{" + entrada + "}"))
 # for x in grafo.values():
