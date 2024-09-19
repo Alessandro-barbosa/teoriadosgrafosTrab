3043cd49bcb1f3d4856e72b538df8deb6654bc77
Alessandro barbosa <66150051+Alessandro-barbosa@users.noreply.github.com>
2024-09-19 06:33:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,43 @@
-def caminhoRED(arvore, no):
-    print(no, end= " ")
-    if arvore[no] != None:
-        if arvore[no][0] != None:
-            caminhoRED(arvore, arvore[no][0])
-        if arvore[no][1] != None:
-            caminhoRED(arvore, arvore[no][1])
+def preOrdem(arvore, no):
+    if no is None:
+        return
+    print(no, end=" ")
+    if arvore[no] is not None:
+        for filho in arvore[no]:
+            if filho is not None:
+                preOrdem(arvore, filho)
+
 
 def ordem(arvore, no):
-    if arvore[no] != None:
-        if arvore[no][0] != None:
-            ordem(arvore, arvore[no][0])  
-        print(no, end = " ")
-        if(arvore[no][1] != None):
-            ordem(arvore, arvore[no][1])    
+    if no is None:
+        return
+
+    # verifica se o nó atual tem filhos
+    if arvore[no] is not None:
+        filhos = arvore[no]
+
+        if len(filhos) > 0 and filhos[0] is not None:
+            ordem(arvore, filhos[0])
+        print(no, end=" ")
+        # filhos restantes
+        for filho in filhos[1:]:
+            if filho is not None:
+                ordem(arvore, filho)
     else:
-        print(no, end = " ")
+        print(no, end=" ")  # não tendo filhos, imprime o nó
+
 
 def posOrdem(arvore, no):
-    if arvore[no] != None:
-        if arvore[no][0] != None:
-            posOrdem(arvore, arvore[no][0])            
-        if arvore[no][1] != None:
-            posOrdem(arvore, arvore[no][1])
-        print(no, end = " ") 
-    else:
-        print(no, end= " ")
+    if no is None:
+        return
+    # nó existente na árvore
+    if no in arvore:
+        filhos = arvore[no]
+        if filhos is not None:
+            for filho in filhos:
+                if filho is not None:
+                    posOrdem(arvore, filho)
 
-def exercicioA():
-    print("Caminho em pré-ordem: ")
-    caminhoRED(arvoreTeste2, 'A')
-    print("\nCaminho em Ordem: ")
-    ordem(arvoreTeste2, 'A')
-    print("\nCaminho em pós-ordem")
-    posOrdem(arvoreTeste2, 'A')
+    # imprime o no raiz após todos
+    print(no, end=" ")
 
-def exercicioB():
-    return None
-
-def exercicioC():
-    return None
-
-def exercicioD():
-    return None
-
-arvoreTeste = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['G', None],
-    'D': None,
-    'E': ['F', None],
-    'F': None,
-    'G': None
-}
-
-arvoreTeste2 = {    
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['G', 'H'],
-    'D': ['I', 'J'],
-    'E': ['K', 'L'],
-    'G': ['O', 'P'],
-    'H': ['Q', 'R'],
-    'I': ['S', None],
-    'J': None,
-    'K': None,
-    'L': ['T', None],
-    'M': [None, 'U'],
-    'N': None,
-    'O': ['V', None],
-    'P': None,
-    'Q': [None, 'W'],
-    'R': None,
-    'S': None,
-    'T': None,
-    'U': None,
-    'V': None,
-    'W': None
-}
-
-exercicioA()
-# entrada = input("Insira a arvore no formato: 'A': ['B', 'C'], 'B': ['A', 'D']\n")
-# grafo = dict(eval("{" + entrada + "}"))
-# for x in grafo.values():
-#     print(x)
